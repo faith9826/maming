695428d662895c7df1d4a43b776a8b63e62a2000
faith9826 <jkmoon9826@gmail.com>
2017-05-17 11:24:08 +0900

    다시

--- application/controllers/Blog.php
+++
@@ -1,7 +0,0 @@
-<?php
-class Blog extends CI_Controller{
-    public function index(){
-        echo "This is Blog";
-    }
-}
-?>
--- application/controllers/Home.php
+++
@@ -1,7 +0,0 @@
-<?php
-class Home extends CI_Controller{
-    public function index(){
-        echo "This is Home";
-    }
-}
-?>
